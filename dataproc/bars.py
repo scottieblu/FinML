189b9e9b0afc18de8b745523072f7a871db8b104
scottieblu <pingu1998@googlemail.com>
2019-03-10 15:48:20 +0100

    Bars notebook.

--- dataproc/bars.py
+++ dataproc/bars.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from numba import jit
@@ -88,6 +89,32 @@ class Bars:
         idx = self.dollar_bars(df, dv_column, m)
         return df.iloc[idx]
 
+    def get_ohlc(self, ref, sub):
+        """
+        fn: get ohlc from custom bars
+
+        # args
+            ref : reference pandas series with all prices
+            sub : custom tick pandas series
+        # returns
+            tick_df : dataframe with ohlc values
+        """
+        ohlc = []
+        for i in tqdm(range(sub.index.shape[0] - 1)):
+            start, end = sub.index[i], sub.index[i + 1]
+            tmp_ref = ref.loc[start:end]
+            max_px, min_px = tmp_ref.max(), tmp_ref.min()
+            o, h, l, c = sub.iloc[i], max_px, min_px, sub.iloc[i + 1]
+            ohlc.append((end, start, o, h, l, c))
+        cols = ['end', 'start', 'open', 'high', 'low', 'close']
+        return pd.DataFrame(ohlc, columns=cols)
+
+    def count_bars(self, df, group='1W', price_col='price'):
+        return df.groupby(pd.Grouper(group))[price_col].count()
+
+    def scale(self, s):
+        return (s - s.min()) / (s.max() - s.min())
+
     @jit(nopython=True)
     def numba_is_close(self, a, b, rel_tol=1e-09, abs_tol=0.0):
         return np.fabs(a - b) <= np.fmax(rel_tol * np.fmax(np.fabs(a), np.fabs(b)), abs_tol)
@@ -109,3 +136,33 @@ class Bars:
             t_bt = self.bt(t[i - 1], t[i], bs[:i - 1])
             bs[i - 1] = t_bt
         return bs[:-1]  # remove last value
+
+    def select_sample_data(self, ref, sub, col, date):
+        """
+        select a sample of data based on date, assumes datetimeindex
+
+        # args
+            ref: pd.DataFrame containing all ticks
+            sub: subordinated pd.DataFrame of prices
+            col: str(), column to use
+            date: str(), date to select
+        # returns
+            xdf: ref pd.Series
+            xtdf: subordinated pd.Series
+        """
+        xdf = ref[col].loc[date]
+        xtdf = sub[col].loc[date]
+        return xdf, xtdf
+
+    def plot_sample_data(self, ref, sub, bar_type, *args, **kwds):
+        f, axes = plt.subplots(3, sharex=True, sharey=True, figsize=(10, 7))
+        ref.plot(*args, **kwds, ax=axes[0], label='price')
+        sub.plot(*args, **kwds, ax=axes[0], marker='X', ls='', label=bar_type)
+        axes[0].legend();
+
+        ref.plot(*args, **kwds, ax=axes[1], label='price', marker='o')
+        sub.plot(*args, **kwds, ax=axes[2], ls='', marker='X',
+                 color='r', label=bar_type)
+
+        for ax in axes[1:]: ax.legend()
+        plt.tight_layout()
